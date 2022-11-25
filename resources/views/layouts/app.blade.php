acb0fb927fe14f50124500845ebcb6db92675e3d
Syun1986 <syun990806@gmail.com>
2022-11-25 16:42:59 +0900

    homeページにログアウトリンクを作成、AppServiceProviderにdefaultStringLengthを追加、layoutsのapp.bladeから@viteの部分(tailwindCSS)を削除

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"/>
