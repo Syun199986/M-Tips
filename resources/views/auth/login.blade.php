73586ac2b38977a679daf484bf54209c46b3b755
Syun1986 <syun990806@gmail.com>
2022-11-25 21:41:39 +0900

    ログアウト後のリダイレクトをログインページに変更、ログインページからレジスターページへのリンクを作成

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,12 +42,17 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
+                
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __('Register') }}
+                </a>
+                
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-3" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
-
+                
                 <x-primary-button class="ml-3">
                     {{ __('Log in') }}
                 </x-primary-button>
