b0888e53756a05a7f56f5396e39c0ac712fa3b63
Syun1986 <syun990806@gmail.com>
2022-12-01 11:20:55 +0900

    all_answersページの見た目修正(仮確定)

--- resources/views/answers/all_answers.blade.php
+++ resources/views/answers/all_answers.blade.php
@@ -9,11 +9,12 @@
 		<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
 			integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 	</head>
 
-	<body>
+	<body class="bg-light">
 		<header>
-			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<nav class="navbar navbar-expand-lg navbar-light bg-white">
 				<div class="container-fluid">
 					<a class="navbar-brand" href="/">M-Tips</a>
 					<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -24,37 +25,44 @@
 					<div class="collapse navbar-collapse" id="navbarSupportedContent">
 						<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 							<li class="nav-item">
-								<a class="nav-link" aria-current="page" href="/">ホーム</a>
+								<a class="nav-link active" aria-current="page" href="/">ホーム</a>
 							</li>
 							<li class="nav-item">
 								<a class="nav-link" aria-current="page" href="/post_question">質問投稿</a>
 							</li>
-							<li class="nav-item">
-								<a class="nav-link" aria-current="page" href="/my_posted_questions">マイ投稿</a>
-							</li>
-							<li class="nav-item">
-								<form method="POST" action="{{ route('logout') }}">
-	                            @csrf
-									<a class="nav-link" aria-current="page" href="route('logout')" 
-											onclick="event.preventDefault();
-	                                           			this.closest('form').submit();">
-										ログアウト
-									</a>
-								</form>
-							</li>
+							@auth
+								<li class="nav-item">
+									<a class="nav-link" aria-current="page" href="/my_posted_questions">マイ投稿</a>
+								</li>
+								<li class="nav-item">
+									<form method="POST" action="{{ route('logout') }}">
+	                            	@csrf
+										<a class="nav-link" aria-current="page" href="route('logout')" 
+												onclick="event.preventDefault();
+	                                            			this.closest('form').submit();">
+											ログアウト
+										</a>
+									</form>
+								</li>
+							@else
+								<li class="nav-item">
+									<a class="nav-link" aria-current="page" href="/login">ログイン</a>
+								</li>
+							@endauth
 						</ul>
-						<!--<form class="d-flex" action="/{{ $question->id }}/all_answers" method="GET">-->
+						<!--<form class="d-flex" action="/" method="GET">-->
 						<!--	@csrf-->
 						<!--	<select class="form-select w-auto" aria-label="Default select example" name="range">-->
 						<!--		<option selected>▼検索範囲</option>-->
 						<!--		<option value="all">全て</option>-->
+						<!--		<option value="title">タイトル</option>-->
 						<!--		<option value="body">本文</option>-->
 						<!--		<option value="user_name">ユーザー名</option>-->
 						<!--	</select>-->
 						<!--	<input class="form-control" type="text" placeholder="検索ワードを入力" aria-label="Search" name="keyword" value="{{ $keyword }}">-->
-						<!--	<input class="btn btn-outline-success mx-2" type="submit" value="検索">-->
+						<!--	<input class="btn btn-outline-dark mx-2" type="submit" value="検索">-->
 						<!--	<button>-->
-						<!--		<a href="/{{ $question->id }}/all_answers" class="btn btn-outline-success">Clear</a>-->
+						<!--		<a href="/" class="btn btn-outline-dark">Clear</a>-->
 						<!--	</button>-->
 						<!--</form>-->
 					</div>
@@ -73,42 +81,80 @@
 			<!--	</select>-->
 			<!--</form>-->
 			@foreach ($answers as $answer)
-				<div class="answers container text-center border border-dark border-2 rounded-3 mb-3">
+				<div class="answers container text-center border-gray-300 border-1 rounded-3 mb-3 px-4 py-2 bg-white">
 					<div class='answer'>
+						
+						
+						<!--<div class="d-flex justify-content-between">-->
+						<!--	<h3 class="row align-items-start">{{ $answer->user_name }} の回答</h2>-->
+						<!--	@foreach($answer->users as $user)-->
+						<!--		@if (Auth::user()->id == $user->id)-->
+						<!--			<div class="d-flex justify-content-between align-items-center">-->
+						<!--					<a href="/answers/{{ $answer->id }}/edit_answer">回答の編集</a>-->
+						<!--			</div>-->
+					 <!-- 			@endif-->
+				  <!--			@endforeach-->
+						<!--</div>-->
+						<!--<div class="d-flex justify-content-between">-->
+						<!--	<p class="row align-items-start">{{ $answer->created_at }}</p>-->
+						<!--	@foreach($answer->users as $user)-->
+						<!--		@if (Auth::user()->id == $user->id)-->
+						<!--			<div class="d-flex justify-content-between">-->
+						<!--				<form action="/answers/{{ $answer->id }}" id="form_{{ $answer->id }}" method="post">-->
+						<!--					@csrf-->
+						<!--					@method('DELETE')-->
+						<!--					<a href="#" onclick="deleteAnswer({{ $answer->id }})" style="color:red">回答の削除</a>-->
+						<!--				</form>-->
+						<!--			</div>-->
+					 <!-- 			@endif-->
+				  <!--			@endforeach-->
+						<!--</div>-->
+						
+						
 						<div class="d-flex justify-content-between">
-							<h3 class="row align-items-start">{{ $answer->user_name }} の回答</h2>
-							@foreach($answer->users as $user)
-								@if (Auth::user()->id == $user->id)
-									<div class="d-flex justify-content-between align-items-center">
-											<a href="/answers/{{ $answer->id }}/edit_answer">回答の編集</a>
-									</div>
-					  			@endif
-				  			@endforeach
+							<!--<h3 class='title row align-items-start mb-0'>{{ $answer->user_name }} の回答</h3>-->
+							<div>
+								<p class="row align-items-start mb-0" style="font-size: 15px; opacity: 0.7;">質問ユーザー：{{ $answer->user_name }}</p>
+								<p class="row align-items-start" style="font-size: 15px; opacity: 0.7;">投稿日時：{{ $answer->created_at }}</p>
+							</div>
+							<div class="d-flex align-items-center">
+								@auth
+									@foreach($answer->users as $user)
+										@if (Auth::user()->id == $user->id)
+											<div class="d-flex justify-content-between">
+												<a type="button" href="/answers/{{ $answer->id }}/edit_answer" class="mr-1 d-flex justify-content-between btn btn-sm btn-outline-success">
+													<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+														<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+														<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+													</svg>
+												</a>
+												<form action="/answers/{{ $answer->id }}" id="form_{{ $answer->id }}" id="form_{{ $answer->id }}" method="post">
+													@csrf
+													@method('DELETE')
+													<a type="botton" href="#" onclick="deleteAnswer({{ $answer->id }})" class="d-flex justify-content-between btn btn-sm btn-outline-danger">
+														<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+															<path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+														</svg>
+													</a>
+												</form>
+											</div>
+						  				@endif
+					  				@endforeach
+				  				@endauth
+				  			</div>
 						</div>
-						<div class="d-flex justify-content-between">
-							<p class="row align-items-start">{{ $answer->created_at }}</p>
-							@foreach($answer->users as $user)
-								@if (Auth::user()->id == $user->id)
-									<div class="d-flex justify-content-between">
-										<form action="/answers/{{ $answer->id }}" id="form_{{ $answer->id }}" method="post">
-											@csrf
-											@method('DELETE')
-											<a href="#" onclick="deleteAnswer({{ $answer->id }})" style="color:red">回答の削除</a>
-										</form>
-									</div>
-					  			@endif
-				  			@endforeach
+						<p class='body row align-items-start text-left' style="font-size: 18px; white-space: pre-wrap;">{{ $answer->body }}</p>
+						<div class="flex justify-center">
+							@if(strrpos($answer->file_path, '.png'))
+							    <img src="{{ $answer->file_path }}">
+							@elseif(strrpos($answer->file_path, '.mp3'))
+								<audio controls src="{{ $answer->file_path }}">
+						            <a href="{{ $answer->file_path }}">
+						            	Download audio
+	            					</a>
+	            				</audio>
+							@endif
 						</div>
-						<p class='body row align-items-start'>{{ $answer->body }}</p>
-						@if(strrpos($answer->file_path, '.png'))
-						    <img src="{{ $answer->file_path }}">
-						@elseif(strrpos($answer->file_path, '.mp3'))
-							<audio controls src="{{ $answer->file_path }}">
-					            <a href="{{ $answer->file_path }}">
-					            	Download audio
-            					</a>
-            				</audio>
-						@endif						
 						<div class="d-flex justify-content-between">
 							<div>
 								<!--<button type="button" class="">★いいね！</button>-->
